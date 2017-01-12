58263b92d9027868c6f1a45582a635e7b645824e
Isaac Park and Kay Kang <pair+parkisaac+gnslrns@gmail.com>
2017-01-12 17:06:04 +0900

    CHIP-6 1. modify StoreDetailActivity Toolbar. 2. when appBar onOffSetChanged title, call Link, map Link control. 3. when back button click, then finish activity.

--- app/src/main/java/com/xeed/cheapnsale/StoreDetailActivity.java
+++ app/src/main/java/com/xeed/cheapnsale/StoreDetailActivity.java
@@ -1,24 +1,39 @@
 package com.xeed.cheapnsale;
 
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.xeed.cheapnsale.adapter.StoreMenuTabPagerAdapter;
 
+
 public class StoreDetailActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener {
+
+    private final static int COLLAPS_LIMIT = -700;
 
     private ViewPager viewPager;
     private Toolbar toolbar;
     private TabLayout tabLayout;
 
     private StoreMenuTabPagerAdapter storeMenuTabPagerAdapter;
+
+    private AppBarLayout appBarLayout;
+    private ImageButton backButton;
+
+    private TextView title;
+    private TextView callLink;
+    private TextView mapLink;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +44,15 @@ public class StoreDetailActivity extends AppCompatActivity implements TabLayout.
         //actionbar에 들어갈 내용을 우리의 view인 toolbar에 적용..
         setSupportActionBar(toolbar);
 
+        appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        backButton = (ImageButton) findViewById(R.id.toolbar_back_button);
+
         viewPager = (ViewPager) findViewById(R.id.view_pager);
         tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        title = (TextView) findViewById(R.id.toolbar_order_text);
+        callLink = (TextView) findViewById(R.id.toolbar_call_link);
+        mapLink = (TextView) findViewById(R.id.toolbar_map_link);
 
         tabLayout.addTab(tabLayout.newTab().setText("메뉴"));
         tabLayout.addTab(tabLayout.newTab().setText("가게정보"));
@@ -43,6 +65,33 @@ public class StoreDetailActivity extends AppCompatActivity implements TabLayout.
         tabLayout.setOnTabSelectedListener(this);
 
         setTabTextStyle(0, Typeface.BOLD);
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+
+                if (verticalOffset > COLLAPS_LIMIT) {
+                    title.setVisibility(View.GONE);
+                    callLink.setVisibility(View.GONE);
+                    mapLink.setVisibility(View.GONE);
+
+                    toolbar.setBackgroundColor(Color.TRANSPARENT);
+                } else {
+                    title.setVisibility(View.VISIBLE);
+                    callLink.setVisibility(View.VISIBLE);
+                    mapLink.setVisibility(View.VISIBLE);
+
+                    toolbar.setBackgroundColor(Color.rgb(255,255,255));
+                }
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     private void setTabTextStyle(int position, int typeface) {
